b0280ef4303f2ae26402f5d0e072e0d36fd245a1
rami11 <rami.serapian@gmail.com>
2021-07-13 15:37:55 -0400

    Modify @CrossOrigin

--- src/main/java/com/rsn/typerbackend/text/TextController.java
+++ src/main/java/com/rsn/typerbackend/text/TextController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("*")
 @RequestMapping("/api")
 public class TextController {
 
